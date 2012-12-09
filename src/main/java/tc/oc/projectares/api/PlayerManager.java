2c4330992adb9bf995d7b1223112e6de7cdaef85
Steve Anton <anxuiz.nx@gmail.com>
2012-12-08 19:36:45 -0800

    Add an example of a static method that would need to be overwritten using reflection.

---
+++ src/main/java/tc/oc/projectares/api/PlayerManager.java
@@ -0,0 +1,9 @@
+package tc.oc.projectares.api;
+
+import javax.annotation.Nullable;
+
+public class PlayerManager {
+    public static @Nullable Player getPlayer(org.bukkit.entity.Player bukkit) {
+        throw new UnsupportedOperationException();
+    }
+}
