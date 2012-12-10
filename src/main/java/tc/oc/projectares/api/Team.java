e8f8ce7d009f209882fc2d92a0d95ea66657362d
mrapple <tony@oc.tc>
2012-12-10 16:17:02 -0600

    Fill out Team methods

--- src/main/java/tc/oc/projectares/api/Team.java
+++ src/main/java/tc/oc/projectares/api/Team.java
@@ -4,6 +4,24 @@ import java.util.Set;
 
 import javax.annotation.Nonnull;
 
+import org.bukkit.ChatColor;
+
 public interface Team {
     @Nonnull Set<Player> getMembers();
+
+    @Nonnull boolean isParticipating();
+
+    @Nonnull boolean isObserving();
+
+    @Nonnull String getName();
+
+    void setName(@Nonnull String newName);
+
+    @Nonnull String getColoredName();
+
+    @Nonnull ChatColor getColor();
+
+    void setColor(@Nonnull ChatColor newColor);
+
+    void broadcast(String message);
 }
