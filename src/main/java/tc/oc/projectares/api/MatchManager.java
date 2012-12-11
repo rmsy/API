05e05150364792c895b404c61c009b86311d71dc
Steve Anton <anxuiz.nx@gmail.com>
2012-12-10 16:56:39 -0800

    Add the MatchManager static class for fetching Match objects.

---
+++ src/main/java/tc/oc/projectares/api/MatchManager.java
@@ -0,0 +1,56 @@
+package tc.oc.projectares.api;
+
+import java.util.Collection;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.bukkit.World;
+
+import tc.oc.projectares.api.backend.BackendManager;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * Provides an entry point for getting {@link Match} instances.
+ */
+public final class MatchManager {
+    private MatchManager() { }
+
+    /**
+     * Gets a list of matches registered with the server.
+     *
+     * @return collection of registered matches
+     */
+    public static @Nonnull Collection<Match> getMatches() {
+        return BackendManager.getBackend().getMatches();
+    }
+
+    /**
+     * Gets the match registered in a single-match environment.
+     *
+     * A single match environment is one where only one match will be running
+     * on the server. In that case, this method should always return a valid
+     * match object, though it is not guaranteed.
+     *
+     * @return only match running or null if there are either no matches
+     *         running or more than one
+     */
+    public static @Nullable Match getMatch() {
+        return BackendManager.getBackend().getMatch();
+    }
+
+    /**
+     * Gets the match registered in a specified world.
+     *
+     * @param world search term for the match
+     *
+     * @return match that is registered in the world or null if there is not one
+     * @throws NullPointerException if world is null
+     */
+    public static @Nullable Match getMatch(@Nonnull World world) {
+        Preconditions.checkNotNull(world, "world");
+
+        return BackendManager.getBackend().getMatch(world);
+    }
+}
--- src/main/java/tc/oc/projectares/api/backend/Backend.java
+++ src/main/java/tc/oc/projectares/api/backend/Backend.java
@@ -1,8 +1,14 @@
 package tc.oc.projectares.api.backend;
 
+import java.util.Collection;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import org.bukkit.World;
+
+import tc.oc.projectares.api.Match;
+import tc.oc.projectares.api.MatchManager;
 import tc.oc.projectares.api.Player;
 import tc.oc.projectares.api.PlayerManager;
 
@@ -19,4 +25,13 @@ import tc.oc.projectares.api.PlayerManager;
 public interface Backend {
     /** @see PlayerManager#getPlayer */
     @Nullable Player getPlayer(@Nonnull org.bukkit.entity.Player bukkit);
+
+    /** @see MatchManager#getMatches */
+    @Nonnull Collection<Match> getMatches();
+
+    /** @see MatchManager#getMatch() */
+    @Nullable Match getMatch();
+
+    /** @see MatchManager#getMatch(World) */
+    @Nullable Match getMatch(@Nonnull World world);
 }
