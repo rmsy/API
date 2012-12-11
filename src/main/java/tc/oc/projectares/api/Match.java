6f3c175613b1c5cfb6b121a25b6fa797c6d5a120
mrapple <tony@oc.tc>
2012-12-10 18:16:40 -0600

    Remove some methods that shouldn't have been exposed

--- src/main/java/tc/oc/projectares/api/Match.java
+++ src/main/java/tc/oc/projectares/api/Match.java
@@ -27,14 +27,6 @@ public interface Match {
 
     @Nonnull Set<Player> getObservingPlayers();
 
-    Player getPlayer(org.bukkit.entity.Player player);
-
-    @Nonnull Player addPlayer(org.bukkit.entity.Player player);
-
-    void removePlayer(org.bukkit.entity.Player player);
-
-    void removeAllPlayers();
-
     void broadcast(String message);
 
     @Nonnull Set<Team> getTeams();
